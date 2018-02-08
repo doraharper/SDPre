69d96ecf8c3a6f04c9c6151491b905c1784ff8b0
Dora Harper <doraeharper@gmail.com>
2018-02-08 15:23:29 -0500

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg {
+
+  public static void main( String[] args ) {
+    System.out.println( "I, Dora Harper, am determined to learn how to code." );
+    System.out.println( "Today's date is Thursday, February 8, 2018." );
+  }
+
+}
