be4f0362f0ceeb94518edc18bf9cd50effde692e
Dora Harper <doraeharper@gmail.com>
2018-02-08 15:46:26 -0500

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt {
+
+  public static void main(String[] args) {
+      System.out.println("+----------------------------+");
+      System.out.println("|                            |");
+      System.out.println("|        CORNER STORE        |");
+      System.out.println("|                            |");
+      System.out.println("|  2018-02-08   03:33PM      |");
+      System.out.println("|                            |");
+      System.out.println("|  Gallons:         10.870   |");
+      System.out.println("|  Price/gallon:   $ 2.649   |");
+      System.out.println("|                            |");
+      System.out.println("|  Fuel total:    $ 28.79    |");
+      System.out.println("|                            |");
+      System.out.println("+----------------------------+");
+  }
+
+}
