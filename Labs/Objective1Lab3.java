bd940a8e524b59f0f8dfa227304717d1f0a33c18
Dora Harper <doraeharper@gmail.com>
2018-02-08 18:44:44 -0500

    complete objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println("Name : Dora Harper");
+    System.out.println("Age : 32");
+    System.out.println("State : Texas");
+  }
+}
