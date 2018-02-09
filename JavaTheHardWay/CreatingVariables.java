4760ac099164a6e615e2f6fd6d816fceee979a80
Dora Harper <doraeharper@gmail.com>
2018-02-08 21:27:55 -0500

    added2nd  String-ex5

--- JavaTheHardWay/CreatingVariables.java
+++ JavaTheHardWay/CreatingVariables.java
@@ -2,12 +2,12 @@ public class CreatingVariables {
   public static void main(String [] args){
     int x, y, age, diapers;
     double seconds, e, checking, savings;
-    String firstName, lastName, title, child;
+    String firstName, lastName, title, child, toddler;
 
     x = 10;
     y = 400;
     age = 39;
-    diapers = 10;
+    diapers = 16;
 
     seconds = 4.71;
     e = 2.71828182845904523536;
@@ -17,7 +17,8 @@ public class CreatingVariables {
     firstName = "Graham";
     lastName = "Mitchell";
     title = "Mr.";
-    child = "baby";
+    child = "baby 1";
+    toddler = "baby 2";
 
     System.out.println("The variable x contains " + x);
     System.out.println("The value " + y + " is stored in the variable y.");
@@ -26,6 +27,6 @@ public class CreatingVariables {
     System.out.println("Hopefully you have more than $" + checking + "!");
     System.out.println("My name's " + title + " " + firstName + lastName);
 
-    System.out.println("When a " + child + " uses about " + diapers + " diapers a day, you might have $" + savings + " in the bank at the end of the month!");
+    System.out.println(diapers + " are used a day between " + child + toddler +". At that rate, you might have $" + savings + " in the bank at the end of the month!");
   }
 }
