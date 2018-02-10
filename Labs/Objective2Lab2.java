bd00b37f651d3e52454239123c05e0a7d8cc7ecb
Dora Harper <doraeharper@gmail.com>
2018-02-09 21:33:50 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,16 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+
+    int age;
+    String name, state;
+
+    age = 47;
+
+    name = "Charles Peterson";
+    state = "Arkansas";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state );
+
+  }
+
+}
