971860a7f05345e3af415887085316820e29c495
Dora Harper <doraeharper@gmail.com>
2018-02-14 15:28:19 -0500

    complete objective2lab5

--- Labs/Objective2Lab5.java
+++ Labs/Objective2Lab5.java
@@ -7,7 +7,7 @@ public class Objective2Lab5 {
 
     hypothenuse = (side1 * side1) + (side2 * side2);
 
-    System.out.println("The hypothenuse of a triangle with sides " + side1 + " and " + side2 + " is ");
+    System.out.print("The hypothenuse of a triangle with sides " + side1 + " and " + side2 + " is ");
     System.out.println(Math.sqrt(hypothenuse));
 
   }
