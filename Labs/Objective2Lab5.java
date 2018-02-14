bc484d54a196567e176a65682eee735c74a93e40
Dora Harper <doraeharper@gmail.com>
2018-02-14 14:48:55 -0500

    complete objective2lab5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,13 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    double side1, side2, hypothenuse;
+
+    side1 = 10.0;
+    side2 = 8.0;
+
+    hypothenuse = (side1 * side1) + (side2 * side2);
+
+    System.out.println(Math.sqrt(hypothenuse));
+
+  }
+}
