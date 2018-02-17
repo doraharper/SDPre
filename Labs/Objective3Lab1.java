e0220ec152375066335fb5d47bdac6e47a9322be
Dora Harper <doraeharper@gmail.com>
2018-02-17 12:25:18 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+
+    System.out.println("Please Typea a word: ");
+    input.next();
+    input.next();
+
+    input.close();
+
+  }
+
+}
