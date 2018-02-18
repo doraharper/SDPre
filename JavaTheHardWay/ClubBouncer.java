feb9feff3425f9239ee743e36f2bcef726cc40a1
Dora Harper <doraeharper@gmail.com>
2018-02-18 17:08:15 -0500

    finish jthw ex17

---
+++ JavaTheHardWay/ClubBouncer.java
@@ -0,0 +1,18 @@
+public class ClubBouncer {
+  public static void main(String[] args) {
+      int age = 22;
+      boolean onGuestList = false;
+      double allure = 7.5;
+      String gender = "F";
+
+      if (onGuestList || age > 21 || (gender.equals("F") && allure >=8) ) {
+        System.out.println("You are allowed to enter the club.");
+      }
+      else {
+        System.out.println("You are not allowed to enter the club.");
+      }
+  }
+
+}
+// Adding println() statement between line 10 and 11 will not compile.
+// gives "else without if" error
