7130800859b1c8397eaa8d69ab586c6cd0dd1148
Dora Harper <doraeharper@gmail.com>
2018-02-25 13:16:23 -0700

    complete objective6lab1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,14 @@
+public class Objective6Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    System.out.print(counter + "\n");
+
+    while (counter > 0) {
+      if (counter < 10) {
+        System.out.print(counter + "\n");
+      }
+      counter = counter - 1;
+    }
+  }
+}
